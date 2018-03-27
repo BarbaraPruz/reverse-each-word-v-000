99d2cdc6a87ac2eebc06ab38ec77002fdd99020c
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-27 00:40:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,7 @@ def reverse_each_word (str)
 #  reverse_arr = []
 #  str.split.each { |word| reverse_arr << word.reverse}
 #  reverse_arr.join(" ")
-  str.split.collect { |word| word.reverse}.join(' ')
+  str.split
+    .collect { |word| word.reverse}
+    .join(' ')
 end
